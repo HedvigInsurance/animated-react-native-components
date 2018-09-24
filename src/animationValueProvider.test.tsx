cca98c3b59da2f72f0cb17f480b172e7580e21d7
Sam Pettersson <sam@sampettersson.com>
2018-09-24 14:45:00 +0200

    v1.0.0

---
+++ src/animationValueProvider.test.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { Animated } from "react-native"
+import { mount } from "enzyme";
+import { AnimationValueProvider } from "./animationValueProvider"
+
+test("it should provide an animated value", () => {
+    const testFn = jest.fn()
+    
+    const Tester = () =>
+        <AnimationValueProvider initialValue={12}>
+            {
+                ({ animatedValue }) => {
+                    expect(animatedValue).toBeDefined()
+                    testFn()
+                    return null
+                }
+            }
+        </AnimationValueProvider>
+
+
+    mount(
+        <Tester />
+    )
+
+    expect(testFn).toHaveBeenCalled()
+})
